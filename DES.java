646eb3dc10362f530fa910c23e7855ec070626c8
Karla Najera <kiliapplered@hotmail.com>
2022-09-30 09:28:14 -0500

    Codigo correcto, bien comentado y cool

--- DES.java
+++ DES.java
@@ -1,9 +1,16 @@
+/* 
+ * @Autor Najera Noyola Karla Andrea
+ * @Fecha 27 de septiembre de 2022
+ * @Descripción Clase que permite el cifrado/descifrado mediante DES.
+*/
+
 public class DES {
 
-    int[] clave;
-    int[] texto;
-    int[] subKey1;
-    int[] subKey2;
+    int[] clave;    // Clave con la que el texto será cifrada. 
+    int[] texto;    // Texto a cifrar o descifrar.
+    int[] subKey1;  // Subllave gennerada 1.
+    int[] subKey2;  // Subllave generada 2.
+    // S-Boxes.
     int[][] S0={ { 1, 0, 3, 2 },
                  { 3, 2, 1, 0 },
                  { 0, 2, 1, 3 },
@@ -13,33 +20,42 @@ public class DES {
                  { 3, 0, 1, 0 },
                  { 2, 1, 0, 3 } };
 
+    /* 
+     * Constructor de la clase.
+     * Recibe la clave y el texto que se cifrará/descifrará.
+     */
     public DES(String claveCadena, String textoCadena){
         this.clave=llenadoArreglos(claveCadena);
         this.texto=llenadoArreglos(textoCadena);
-        //imprimirArreglo(clave);
-        //imprimirArreglo(texto);
     }
 
+    /* 
+     * Función de llenado de arreglos.
+     * Recibe una cadena y convierte a arreglo de enteros el contenido. 
+     */
     public int[] llenadoArreglos(String cadena){
         int[] aux=new int[cadena.length()];
         for(int i=0; i<cadena.length(); i++){
-            aux[i]=(int)cadena.charAt(i);
-            if(aux[i]==48){
-                aux[i]=0;
-            } else {
-                aux[i]=1;
-            }
+            aux[i]=Integer.parseInt(""+cadena.charAt(i));
         }
         return aux;
     }
 
+    /* 
+     * Función de impresión de arreglos.
+     * Recibe un arreglo e imprime el contenido mediante un ciclo. 
+     */
     public void imprimirArreglo(int[] arreglo){
         for(int i=0; i<arreglo.length; i++){
-            System.out.print(arreglo[i]+" ");
+            System.out.print(arreglo[i]);
         }
         System.out.println("");
     }
 
+    /* 
+     * Permutador de arreglos.
+     * Recibe un arreglo y lo acomoda según lo indicado en otro arreglo. 
+     */
     public int[] permutacion(int[] arregloPerm, int[] arregloPos){
         int[] aux=new int[arregloPos.length];
         for(int i=0; i<arregloPos.length; i++){
@@ -48,6 +64,10 @@ public class DES {
         return aux;
     }
 
+    /* 
+     * Divisor de arreglos.
+     * Recibe un arreglo y lo divide a la mitad, devolviendo la parte indicada. 
+     */
     public int[] division(int[] arreglo, int parte){
         int aux[]=new int[arreglo.length/2];
         int n=0;
@@ -60,6 +80,10 @@ public class DES {
         return aux;
     }
 
+    /* 
+     * Desplazador de arreglos.
+     * Recibe un arreglo y lo mueve n posiciones como le sea indicado. 
+     */
     public int[] desplazamiento(int[] arreglo, int pos){
         while(pos>0){
             int temp=arreglo[0];
@@ -72,50 +96,23 @@ public class DES {
         return arreglo;
     }
 
+    /* 
+     * Unión de arreglos.
+     * Recibe 2 arreglos y los une en uno mismo. 
+     */
     public int[] union(int[] arreglo1, int[] arreglo2){
         int[] aux=new int[arreglo1.length*2];
         for(int i=0; i<arreglo1.length; i++){
             aux[i]=arreglo1[i];
             aux[i+(arreglo1.length)]=arreglo2[i];
-            //System.out.println(arreglo1[i]+ " "+arreglo2[i]);
         }
         return aux;
     }
 
-    public void obtencionSubkeys(){
-        // Creación de las subclaves
-        // Paso 1: Permutación
-        int[] arregloPos1={2,4,1,6,3,9,0,8,7,5};
-        int[] arregloPermutado1=permutacion(clave, arregloPos1);
-        //imprimirArreglo(arregloPermutado1);
-        // Paso 2: División
-        int[] sub1=division(arregloPermutado1, 1);
-        int[] sub2=division(arregloPermutado1, 2);
-        //imprimirArreglo(sub1);
-        //imprimirArreglo(sub2);
-        // Paso 3: Desplazamiento de bit a la izquierda
-        sub1=desplazamiento(sub1, 1);
-        sub2=desplazamiento(sub2, 1);
-        //imprimirArreglo(sub1);
-        //imprimirArreglo(sub2);
-        // Paso 4: Se unen las 2 cadenas anteriores
-        int[] preKey1=union(sub1, sub2);
-        //imprimirArreglo(preKey1);
-        // Paso 5: Se obtiene la subKey1
-        int[] arregloPos2={5,2,6,3,7,4,9,8};
-        this.subKey1=permutacion(preKey1, arregloPos2);
-        // Paso 6: Desplazamiento de 2 bits a la izquierda del paso 3
-        sub1=desplazamiento(sub1, 2);
-        sub2=desplazamiento(sub2, 2);
-        // Paso 7: Se unen las 2 cadenas anteriores
-        int[] preKey2=union(sub1, sub2);
-        // Paso 8: Se obtiene la subKey2
-        this.subKey2=permutacion(preKey2, arregloPos2);
-        // Se ha completado el proceso de obtención de subllaves
-        //imprimirArreglo(subKey1);
-        //imprimirArreglo(subKey2);
-    }
-
+    /* 
+     * Transformador de valores binarios.
+     * Recibe un entero y lo transforma a una cadena binaria. 
+     */
     public String binario(int val){
         if (val == 0)
             return "00";
@@ -127,18 +124,59 @@ public class DES {
             return "11";
     }
 
+    /* 
+     * Obtención de subkeys.
+     * Realiza la obtención de las subkeys a partir de la key proporcionada por el usuario. 
+     */
+    public void obtencionSubkeys(){
+
+        // Paso 1: Permutación
+        int[] arregloPos1={2,4,1,6,3,9,0,8,7,5};
+        int[] arregloPermutado1=permutacion(clave, arregloPos1);
+
+        // Paso 2: División
+        int[] sub1=division(arregloPermutado1, 1);
+        int[] sub2=division(arregloPermutado1, 2);
+
+        // Paso 3: Desplazamiento de bit a la izquierda
+        sub1=desplazamiento(sub1, 1);
+        sub2=desplazamiento(sub2, 1);
+
+        // Paso 4: Se unen las 2 cadenas anteriores
+        int[] preKey1=union(sub1, sub2);
+
+        // Paso 5: Se obtiene la subKey1
+        int[] arregloPos2={5,2,6,3,7,4,9,8};
+        this.subKey1=permutacion(preKey1, arregloPos2);
+
+        // Paso 6: Desplazamiento de 2 bits a la izquierda del paso 3
+        sub1=desplazamiento(sub1, 2);
+        sub2=desplazamiento(sub2, 2);
+
+        // Paso 7: Se unen las 2 cadenas anteriores
+        int[] preKey2=union(sub1, sub2);
+
+        // Paso 8: Se obtiene la subKey2
+        this.subKey2=permutacion(preKey2, arregloPos2);
+
+        // Se ha completado el proceso de obtención de subllaves
+    }
+
+    /* 
+     * Obtención de subkeys.
+     * Realiza la obtención de las subkeys a partir de la key proporcionada por el usuario. 
+     */
     public int[] feistel(int[] arregloInicial, int sk, int vf){
+
         // Se divide en secciones left y right 
         int[] left=division(arregloInicial, 1);
         int[] right=division(arregloInicial, 2);
-        //imprimirArreglo(left);
-        //imprimirArreglo(right);
 
         // Mezcla de right con Subkey1
-        // Expansion
+        // Expansion de Right
         int[] arregloPos2={3,0,1,2,1,2,3,0};
         int[] expRight=permutacion(right, arregloPos2);
-        //imprimirArreglo(expRight);
+
         // Aplicación de XOR con Subkey correspondiente
         int[] xor1=new int[expRight.length];
         if(sk==1){
@@ -150,14 +188,12 @@ public class DES {
                 xor1[i]=(expRight[i]^this.subKey2[i]);
             }
         }
-        //imprimirArreglo(xor1);
+
         // Division previa a S-Box
         int[] temp1=division(xor1, 1);
         int[] temp2=division(xor1, 2);
-        //imprimirArreglo(temp1);
-        //imprimirArreglo(temp2);
 
-        //S-Box S0
+        // S-Box S0
         int row, col, val;
         row=Integer.parseInt(""+temp1[0]+temp1[3], 2);
         col=Integer.parseInt(""+temp1[1]+temp1[2], 2);
@@ -165,89 +201,88 @@ public class DES {
         String val1=binario(val);
         int[] cad1=llenadoArreglos(val1);
 
-        //S-Box S1
+        // S-Box S1
         row=Integer.parseInt(""+temp2[0]+temp2[3], 2);
         col=Integer.parseInt(""+temp2[1]+temp2[2], 2);
         val=S1[row][col];
         String val2=binario(val);
         int[] cad2=llenadoArreglos(val2);
 
-        //Concatenación de resultados
+        // Concatenación de resultados
         int[] concat=union(cad1, cad2);
-        //imprimirArreglo(concat);
         
-        //Permutacion ----------------------- 
+        // Permutacion
         int[] arregloPos3={1,3,2,0};
         int[] perm1=permutacion(concat, arregloPos3);
-        //imprimirArreglo(perm1);
 
-        //XOR con la mitad izquierda
+        // XOR con la mitad izquierda
         int[] xor2=new int[perm1.length];
         for(int i=0; i<perm1.length; i++){
             xor2[i]=(perm1[i]^left[i]);
         }
-        //imprimirArreglo(xor2);
 
-        //Concatenación con mitad derecha e intercmabio
+        // Concatenación con mitad derecha e intercmabio
         int[] concat2=new int[xor2.length*2];
         if(vf==1){
             concat2=union(right, xor2);
         } else {
             concat2=union(xor2, right);
         }
-        //imprimirArreglo(concat2);
         return concat2;
     }
 
-    public void cifrado(){
+    public int[] cifrado(){
+
+        // Obtención de subKeys
+        obtencionSubkeys();
+
         // Permutación inicial
         int[] arregloPos1={1,5,2,0,3,7,4,6};
         int[] arregloPermutado1=permutacion(this.texto, arregloPos1);
-        //imprimirArreglo(arregloPermutado1);
 
-        //Inicio Feistel
-        int[] concat2=feistel(arregloPermutado1, 1, 1);
+        //  Aplicación de Feistel
+        int[] concat=feistel(arregloPermutado1, 1, 1);
 
-        // Fin feistel
+        // Intercambio
+        int[] aux1=division(concat, 1);
+        int[] aux2=division(concat, 2);
+        
+        // Concatenación
+        concat=union(aux1, aux2);
+        int[] concat2=feistel(concat, 2, 2);
 
-        //Intercambio
-        int[] aux1=division(concat2, 1);
-        int[] aux2=division(concat2, 2);
-        concat2=union(aux1, aux2);
-
-        int[] concat4=feistel(concat2, 2, 2);
-
-        //Permutacion final
+        // Permutacion final
         int[] arregloPerm={3,0,2,4,6,1,7,5};
-        int[] cadenaFinal=permutacion(concat4, arregloPerm);
+        int[] cadenaFinal=permutacion(concat2, arregloPerm);
 
-        imprimirArreglo(cadenaFinal);
-
+        // Se devuelve el mensaje cifrado
+        return cadenaFinal;
     }
 
-    public void descifrado(){
+    public int[] descifrado(){
+        // Obtención de subKeys
+        obtencionSubkeys();
         // Permutación inicial
         int[] arregloPos1={1,5,2,0,3,7,4,6};
         int[] arregloPermutado1=permutacion(this.texto, arregloPos1);
-        //imprimirArreglo(arregloPermutado1);
 
-        //Inicio Feistel
+        // Aplicación de Feistel
         int[] concat2=feistel(arregloPermutado1, 2, 1);
 
-        // Fin feistel
-
-        //Intercambio
+        // Intercambio
         int[] aux1=division(concat2, 1);
         int[] aux2=division(concat2, 2);
-        concat2=union(aux1, aux2);
 
+        // Concatenación
+        concat2=union(aux1, aux2);
         int[] concat4=feistel(concat2, 1, 2);
 
-        //Permutacion final
+        // Permutacion final
         int[] arregloPerm={3,0,2,4,6,1,7,5};
         int[] cadenaFinal=permutacion(concat4, arregloPerm);
 
-        imprimirArreglo(cadenaFinal);
+        // Se devuelve el mensaje descifrado
+        return cadenaFinal;
     }
     
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 /* 
  * @Autor Najera Noyola Karla Andrea
- * @Fecha 25 de septiembre de 2022
- * @Descripción Clase principal que solicita datos al usuario para aplicar RC4.
+ * @Fecha 27 de septiembre de 2022
+ * @Descripción Clase principal que solicita datos al usuario para aplicar DES.
 */
 
 import java.util.Scanner;
@@ -17,7 +17,7 @@ public class Main {
         Scanner sc= new Scanner(System.in);
         String key="";
         String texto="";
-        String mensaje="";
+        int[] mensaje;
 
         // Datos de bienvenida
         System.out.println(" **********     DES Algorithm     ********** ");
@@ -25,46 +25,39 @@ public class Main {
 
         int op=0;
         // Ciclo que permite la repetición del menú de opciones
-        //while(op!=3){
+        while(op!=3){
             System.out.println("\n Select an option: ");
             System.out.println("  1.-Encrypt ");
             System.out.println("  2.-Decrypt ");
             System.out.println("  3.-Exit \n ");
             
             // Selección del usuario
-            //op=sc.nextInt();
-            //sc.nextLine();
-            op=1;
+            op=sc.nextInt();
+            sc.nextLine();
             
 
             System.out.println("");
             // Menú del programa
             switch(op){ 
                 case 1: // Encriptado del mensaje
-                    /*System.out.print("Key: ");
+                    System.out.print("Key: ");
                     key=sc.nextLine();
                     System.out.print("Plaintext: ");
-                    texto=sc.nextLine();*/
-                    DES cifrado= new DES("0000011111", "01010101");
-                    cifrado.obtencionSubkeys();
-                    cifrado.cifrado();
-                    System.out.println("");
-                    DES descifrado = new DES("0000011111", "11000100");
-                    descifrado.obtencionSubkeys();
-                    descifrado.descifrado();
-
-                    //Rc4 encriptado=new Rc4(key, texto);
-                    //mensaje = encriptado.encriptar();
-                    //System.out.println("Encrypted Message: "+mensaje);
+                    texto=sc.nextLine();
+                    DES cifrado= new DES(key, texto);
+                    mensaje=cifrado.cifrado();
+                    System.out.print("Ciphertext: ");
+                    cifrado.imprimirArreglo(mensaje);
                     break;
                 case 2: // Desencriptado del mensaje
                     System.out.print("Key: ");
                     key=sc.nextLine();
-                    System.out.print("Encrypted Text: ");
+                    System.out.print("Ciphertext: ");
                     texto=sc.nextLine();
-                    //Rc4 desencriptado=new Rc4(key, texto);
-                    //mensaje = desencriptado.desencriptar();
-                    //System.out.println("Decrypted Message: "+mensaje);
+                    DES descifrado= new DES(key, texto);
+                    mensaje=descifrado.descifrado();
+                    System.out.print("Plaintext: ");
+                    descifrado.imprimirArreglo(mensaje);
                     break;
                 case 3: // Salida del programa
                     System.out.println("Goodbye.");
@@ -75,7 +68,7 @@ public class Main {
             }
             System.out.println("\nPress Enter to continue...");
             sc.nextLine();
-        //}
+        }
         sc.close();
     }
 }
